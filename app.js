c6b414db79f0802716537cd7c22e58961af1d6f8
Abdul Rafay <abdulrafaykhatana@gmail.com>
2025-02-27 01:09:29 +0500

    Connected MongoDB

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+const Test = require('./models/Test');
 
 const authRoutes = require('./routes/auth');
 const supervisorRoutes = require('./routes/supervisor');
@@ -13,6 +16,18 @@ const app = express();
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(async () => {
+    console.log('MongoDB connected successfully');
+
+    // Test data to ensure connection
+    const testEntry = new Test({ name: 'MongoDB Test by khizar\'s PC' });
+    await testEntry.save();
+    console.log('Test data added:', testEntry);
+  })
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 // Routes
 app.use('/api/v1/auth', authRoutes);
