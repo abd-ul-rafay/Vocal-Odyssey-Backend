4d0f3cf8b9c175553cada7a5c3b5bcd104b9e381
Abdul Rafay <abdulrafaykhatana@gmail.com>
2025-06-06 16:45:21 +0500

    Removed case sensitivity errors

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/user");
 const Otp = require("../models/Otp");
 const { generatePasswordRecoveryEmail } = require("../utils/emailTemplates");
 const bcrypt = require("bcrypt");
